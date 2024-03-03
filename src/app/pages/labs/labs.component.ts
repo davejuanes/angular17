2bdb0e2727436de5c199297f37b0968cdbec7f35
DaveJuanes <dmjuanes_@hotmail.es>
2024-03-02 21:24:28 -0400

    [CLASE 6] EVENT BINDING KEYDOWN

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -32,6 +32,10 @@ export class LabsComponent {
 
   changeHandler(event: Event) {
     console.log(event);
+  }
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement
+    console.log(input.value);
     
   }
 }
