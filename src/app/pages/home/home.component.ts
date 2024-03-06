9b0e009025aaae2901e2939a28005f5a1d323047
DaveJuanes <dmjuanes_@hotmail.es>
2024-03-05 22:29:02 -0400

    [CLASE 11] CHANGE TO SIGNAL TASK[] AND PENDING CHANGE COMPLETED STATE CON ARRAY OF OBJECTS

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Task } from '../../models/task.model'
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -9,18 +11,47 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasksSignal = signal([
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componentes'
+  tasksSignal = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Implementar estructura',
+      completed: false
+    },
+    
   ])
 
   changeHandler(event: Event) {
     const input = event.target as  HTMLInputElement
     const newTask = input.value
-    this.tasksSignal.update((tasksSignal) => [...tasksSignal, newTask]) // No mutar sino crear nuevos estados
-    // {{  }} Strign interpolation
+    // this.tasksSignal.update((tasksSignal) => [...tasksSignal, newTask]) // No mutar sino crear nuevos estados
+    // {{  }} String interpolation
     // ... Spread operator
+    this.addTask(newTask)
+  }
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    }
+    this.tasksSignal.update((tasksSignal) => [...tasksSignal, newTask])
+  }
+  completedTask(id: number) {
+    const taskToUpdate = this.tasksSignal.update(task => task.id === id);
+
+    const task = this.tasksSignal.update((this.tasksSignal.id == id) => task.completed.update = true)
+
+    console.log(task);
   }
   deleteTask(index: number) {
     this.tasksSignal.update((tasksSignal) => tasksSignal.filter((task, position) => position !== index))
